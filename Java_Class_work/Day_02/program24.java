a1627e43c7ce1b73a97c847619d8e1cb9afeb3d7
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-23 22:53:31 +0530

    Update program24.java

--- Java_Class_work/Day_02/program24.java
+++ Java_Class_work/Day_02/program24.java
@@ -3,7 +3,7 @@ class Marvellous
     public void Display() 
     { 
         int iCnt = 0;
-
+ 
         for(iCnt = 1; iCnt <= 4; iCnt++)
         {
             System.out.println("Jay Ganesh...");
