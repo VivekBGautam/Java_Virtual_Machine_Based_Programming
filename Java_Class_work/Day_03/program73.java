837aefa04a529b863b1491e7ea58c94f8d9205ba
Vivek Gautam <vivekbgautam@gmail.com>
2026-03-27 22:27:52 +0530

    Add files via upload

---
+++ Java_Class_work/Day_03/program73.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class program73
+{
+    public static void main(String A[])
+    {
+        int iNo = 0;
+        int i = 0;
+
+        System.out.println("Enter number :");
+
+        Scanner sobj = new Scanner(System.in); 
+        iNo = sobj.nextInt();
+
+        for(i = 1; i <= iNo/2; i++)
+        {
+            if((iNo % i) == 0)
+            {
+                System.out.println(i);
+            }
+        }
+
+        sobj.close();
+    }
+}
